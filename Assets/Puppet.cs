3daad6994407c1f6c0a9142f30c69b54d28e54f2
Keijiro Takahashi <keijiro@gmail.com>
2018-01-30 22:51:11 +0900

    more groovy move

--- Assets/Puppet.cs
+++ Assets/Puppet.cs
@@ -19,22 +19,24 @@ public class Puppet : MonoBehaviour
 
     void OnAnimatorIK(int layerIndex)
     {
-        _animator.SetIKPosition(AvatarIKGoal.LeftFoot, new Vector3(0.2f, 0.1f, 0));
+        var footTime = Time.time * 4;
+
+        _animator.SetIKPosition(AvatarIKGoal.LeftFoot, new Vector3(0.2f, 0.3f * Mathf.Max(0, Mathf.Sin(footTime)), 0));
         _animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
 
-        _animator.SetIKPosition(AvatarIKGoal.RightFoot, new Vector3(-0.2f, 0.1f, 0));
+        _animator.SetIKPosition(AvatarIKGoal.RightFoot, new Vector3(-0.2f, 0.3f * Mathf.Max(0, Mathf.Sin(footTime + Mathf.PI)), 0));
         _animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
 
-        _animator.SetIKPosition(AvatarIKGoal.LeftHand, new Vector3(0.5f, 1.3f, 0));
+        _animator.bodyPosition = new Vector3(Mathf.Sin(footTime) * -0.2f, 0.9f + Mathf.Cos(footTime * 2) * 0.1f, 0);
+        _animator.bodyRotation = Quaternion.AngleAxis(180, Vector3.up) * _bodyNoise.Rotation(1, 10, 10, 10);
+
+        _animator.SetIKPosition(AvatarIKGoal.LeftHand, _animator.bodyPosition + new Vector3(0.3f, 0.2f, -0.2f) + Vector3.Scale(_bodyNoise.Vector(2), new Vector3(0.1f, 0.3f, 0.1f)));
         _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
 
-        _animator.SetIKPosition(AvatarIKGoal.RightHand, new Vector3(-0.5f, 1.3f, 0));
+        _animator.SetIKPosition(AvatarIKGoal.RightHand, _animator.bodyPosition + new Vector3(-0.3f, 0.2f, -0.2f) + Vector3.Scale(_bodyNoise.Vector(3), new Vector3(0.1f, 0.3f, 0.1f)));
         _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
 
-        _animator.bodyPosition = new Vector3(0, 0.8f, 0) + _bodyNoise.Vector(0) * 0.2f;
-        _animator.bodyRotation = Quaternion.AngleAxis(180, Vector3.up) * _bodyNoise.Rotation(1, 30, 30, 30);
-
-        _animator.SetLookAtPosition(new Vector3(0, 0, -1));
+        _animator.SetLookAtPosition(new Vector3(0, 0, -1) + _bodyNoise.Vector(4));
         _animator.SetLookAtWeight(1);
     }
 }
