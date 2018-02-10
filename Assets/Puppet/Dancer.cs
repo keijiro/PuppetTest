58da6b74aece0472a566d3bc33062abf7fff7e43
Keijiro Takahashi <keijiro@gmail.com>
2018-02-10 23:38:58 +0900

    refactoring

--- Assets/Puppet/Dancer.cs
+++ Assets/Puppet/Dancer.cs
@@ -5,192 +5,253 @@ namespace Puppet
 {
     public class Dancer : MonoBehaviour
     {
+        #region Editable fields
+
         [SerializeField] float _stepFrequency = 2;
         [SerializeField] float _stride = 0.4f;
         [SerializeField] float _stepHeight = 0.3f;
         [SerializeField] float _stepAngle = 90;
         [SerializeField] float _bodyHeight = 0.9f;
-        [SerializeField] float _bodyUndulation = 10;
+        [SerializeField] float _bodyPositionNoise = 0.1f;
+        [SerializeField] float _bodyRotationNoise = 30;
         [SerializeField] Vector3 _handPosition = new Vector3(0.3f, 0.3f, -0.2f);
-        [SerializeField] float _handMove = 0.2f;
+        [SerializeField] float _handPositionNoise = 0.3f;
         [SerializeField] float _headMove = 3;
         [SerializeField] float _noiseFrequency = 1.1f;
         [SerializeField] int _randomSeed = 123;
 
-        float SmoothStep(float x)
-        {
-            return x * x * (3 - 2 * x);
-        }
+        #endregion
 
-        int StepCount { get { return Mathf.FloorToInt(_stepTime); } }
-        int StepSeed { get { return StepCount * 100; } }
-        float StepFracTime { get { return _stepTime - Mathf.Floor(_stepTime); } }
-        float StepAngle { get { return _hash.Range(0.5f, 1.0f, StepSeed) * _stepAngle * (_hash.Value01(StepSeed + 1) > 0.5f ? 1 : -1); } }
-        bool PivotIsLeft { get { return (StepCount & 1) == 0; } }
-
-        Quaternion StepRotation {
-            get { return Quaternion.AngleAxis(StepAngle, Vector3.up); }
-        }
-
-        Quaternion StepFracRotation {
-            get { return Quaternion.AngleAxis(StepAngle * StepFracTime, Vector3.up); }
-        }
-
-        Vector3 FootBias { get { return Vector3.up * _animator.leftFeetBottomHeight; } }
-
-        Vector3 LeftFootPos {
-            get {
-                if (PivotIsLeft) return _feet[0] + FootBias;
-                var pos = _feet[1] + StepFracRotation * (_feet[0] - _feet[1]);
-                var up = Mathf.Sin(SmoothStep(StepFracTime) * Mathf.PI) * _stepHeight * Vector3.up;
-                return FootBias + pos + up;
-            }
-        }
-
-        Vector3 RightFootPos {
-            get {
-                if (!PivotIsLeft) return _feet[1] + FootBias;
-                var pos = _feet[0] + StepFracRotation * (_feet[1] - _feet[0]);
-                var up = Mathf.Sin(SmoothStep(StepFracTime) * Mathf.PI) * _stepHeight * Vector3.up;
-                return FootBias + pos + up;
-            }
-        }
-
-        Vector3 BodyPosition {
-            get {
-                var pivot = Vector3.Lerp(RightFootPos, LeftFootPos, Mathf.Sin(StepFracTime * Mathf.PI + (PivotIsLeft ? 0 : Mathf.PI)) * 0.5f + 0.5f);
-                pivot.y = 0;
-                return pivot + Vector3.up * (_bodyHeight + Mathf.Cos(StepFracTime * Mathf.PI * 4) * _stepHeight / 2);
-            }
-        }
-
-        Quaternion BodyRotation {
-            get {
-                var right = RightFootPos - LeftFootPos;
-                right.y = 0;
-                return
-                    Quaternion.AngleAxis(-90, Vector3.up) *
-                    Quaternion.LookRotation(right.normalized) *
-                    _bodyNoise.Rotation(1, _bodyUndulation);
-            }
-        }
-
-        Vector3 LeftHandPosition {
-            get {
-                var pos = _handPosition;
-                pos.x *= -1;
-
-                pos = _animator.bodyRotation * pos;
-                pos += _animator.bodyPosition;
-                pos += _bodyNoise.Vector(3) * _handMove;
-
-                pos = _spineMatrixInv * new Vector4(pos.x, pos.y, pos.z, 1);
-                pos.z = Mathf.Max(pos.z, 0.2f);
-                pos.y = Mathf.Max(pos.y, 0.2f);
-                pos = _spineMatrix * new Vector4(pos.x, pos.y, pos.z, 1);
-
-                return pos;
-            }
-        }
-
-        Vector3 RightHandPosition {
-            get {
-                var pos = _handPosition;
-
-                pos = _animator.bodyRotation * pos;
-                pos += _animator.bodyPosition;
-                pos += _bodyNoise.Vector(4) * _handMove;
-
-                pos = _spineMatrixInv * new Vector4(pos.x, pos.y, pos.z, 1);
-                pos.z = Mathf.Max(pos.z, -0.2f);
-                pos.y = Mathf.Max(pos.y, 0.2f);
-                pos = _spineMatrix * new Vector4(pos.x, pos.y, pos.z, 1);
-
-                return pos;
-            }
-        }
-
-        Vector3 LookAtPosition {
-            get {
-                var pos = _bodyNoise.Vector(5) * _headMove;
-                pos.z = 2;
-                return _animator.bodyRotation * pos + _animator.bodyPosition;
-            }
-        }
+        #region Private variables
 
         Animator _animator;
 
         XXHash _hash;
-        NoiseGenerator _bodyNoise;
+        NoiseGenerator _noise;
 
-        float _stepTime;
+        // Foot positions
         Vector3[] _feet = new Vector3[2];
 
-        Matrix4x4 _spineMatrix;
-        Matrix4x4 _spineMatrixInv;
+        // Timer for step animation
+        float _step;
 
-        void OnDrawGizmos()
+        // Transformation matrices of the chest bone. Used to calculate the
+        // hand positions. These matrices are unavailable in OnAnimatorIK, so
+        // cache them in Update.
+        Matrix4x4 _chestMatrix, _chestMatrixInv;
+
+        #endregion
+
+        #region Local math functions
+
+        static Vector3 SetY(Vector3 v, float y)
         {
-            if (!Application.isPlaying) return;
-
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(_feet[0], 0.05f);
-            Gizmos.DrawWireSphere(_feet[1], 0.05f);
-            Gizmos.DrawLine(_feet[0], _feet[1]);
-
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(LeftFootPos, 0.05f);
-            Gizmos.DrawWireSphere(RightFootPos, 0.05f);
-            Gizmos.DrawLine(LeftFootPos, RightFootPos);
+            v.y = y;
+            return v;
         }
+
+        static float SmoothStep(float x)
+        {
+            return x * x * (3 - 2 * x);
+        }
+
+        #endregion
+
+        #region Local properties and functions for foot animation
+
+        // What number the current step is
+        int StepCount { get { return Mathf.FloorToInt(_step); } }
+
+        // Progress of the current step
+        float StepTime { get { return _step - Mathf.Floor(_step); } }
+
+        // Random seed for the current step
+        int StepSeed { get { return StepCount * 100; } }
+
+        // Is the pivot in the current step the left foot?
+        bool PivotIsLeft { get { return (StepCount & 1) == 0; } }
+
+        // Is the current step a left turn or a right turn?
+        float StepSign { get {
+            return _hash.Value01(StepSeed) > 0.5f ? 1 : -1;
+        } }
+
+        // Angle of the pivot rotation in the current step.
+        float StepAngle { get {
+            return _hash.Range(0.5f, 1.0f, StepSeed + 1) * _stepAngle * StepSign;
+        } }
+
+        // Pivot rotation at the end of the current step.
+        Quaternion StepRotationFull { get {
+            return Quaternion.AngleAxis(StepAngle, Vector3.up);
+        } }
+
+        // Pivot rotation at the current time.
+        Quaternion StepRotation { get {
+            return Quaternion.AngleAxis(StepAngle * StepTime, Vector3.up);
+        } }
+
+        // The original height of the foot point.
+        Vector3 FootBias { get {
+            return Vector3.up * _animator.leftFeetBottomHeight;
+        } }
+
+        // Calculates the foot position
+        Vector3 GetFootPosition(int index)
+        {
+            var thisFoot = _feet[index];
+            var thatFoot = _feet[(index + 1) & 1];
+
+            // If it's the pivot foot, return it immediately.
+            if (PivotIsLeft ^ (index == 1)) return thisFoot + FootBias;
+
+            // Calculate the relative position from the pivot foot.
+            var rp = StepRotation * (thisFoot - thatFoot);
+
+            // Vertical move: Sine wave with smooth step
+            var up = Mathf.Sin(SmoothStep(StepTime) * Mathf.PI) * _stepHeight;
+
+            return thatFoot + rp + up * Vector3.up + FootBias;
+        }
+
+        Vector3 LeftFootPosition { get { return GetFootPosition(0); } }
+        Vector3 RightFootPosition { get { return GetFootPosition(1); } }
+
+        #endregion
+
+        #region Local properties and functions for body animation
+
+        // Body (hip) position
+        Vector3 BodyPosition {
+            get {
+                // Move so that it keeps the center of mass on the pivot fott.
+                var theta = (StepTime + (PivotIsLeft ? 0 : 1)) * Mathf.PI;
+                var right = (1 - Mathf.Sin(theta)) / 2;
+                var pos = Vector3.Lerp(LeftFootPosition, RightFootPosition, right);
+
+                // Vertical move: Two wave while one step. Add noise.
+                var y = _bodyHeight + Mathf.Cos(StepTime * Mathf.PI * 4) * _stepHeight / 2;
+                y += _noise.Value(0) * _bodyPositionNoise;
+
+                return SetY(pos, y);
+            }
+        }
+
+        // Body (hip) rotation
+        Quaternion BodyRotation {
+            get {
+                // Base rotation
+                var rot = Quaternion.AngleAxis(-90, Vector3.up);
+
+                // Right vector from the foot positions
+                var right = SetY(RightFootPosition - LeftFootPosition, 0);
+
+                // Horizontal rotation from the right vector.
+                rot *= Quaternion.LookRotation(right.normalized);
+
+                // Add noise.
+                return rot * _noise.Rotation(1, _bodyRotationNoise);
+            }
+        }
+
+        #endregion
+
+        #region Local properties and functions for upper body animation
+
+        // Calculates the hand position
+        Vector3 GetHandPosition(int index)
+        {
+            var isLeft = (index == 0);
+
+            // Relative position of the hand.
+            var pos = _handPosition;
+            if (isLeft) pos.x *= -1;
+
+            // Apply the body (hip) transform.
+            pos = _animator.bodyRotation * pos + _animator.bodyPosition;
+
+            // Add noise.
+            pos += _noise.Vector(2 + index) * _handPositionNoise;
+
+            // Clamping in the local space of the chest bone.
+            pos = _chestMatrixInv * new Vector4(pos.x, pos.y, pos.z, 1);
+            pos.y = Mathf.Max(pos.y, 0.2f);
+            pos.z = isLeft ? Mathf.Max(pos.z, 0.2f) : Mathf.Min(pos.z, -0.2f);
+            pos = _chestMatrix * new Vector4(pos.x, pos.y, pos.z, 1);
+
+            return pos;
+        }
+
+        Vector3 LeftHandPosition { get { return GetHandPosition(0); } }
+        Vector3 RightHandPosition { get { return GetHandPosition(1); } }
+
+        // Look at position (for head movement)
+        Vector3 LookAtPosition {
+            get {
+                var pos = _noise.Vector(5) * _headMove;
+                pos.z = 2;
+                return _animator.bodyPosition + _animator.bodyRotation * pos;
+            }
+        }
+
+        #endregion
+
+        #region MonoBehaviour implementation
 
         void Start()
         {
             _animator = GetComponent<Animator>();
 
+            // Random number/noise generators
             _hash = new XXHash(_randomSeed);
-            _bodyNoise = new NoiseGenerator(_noiseFrequency);
+            _noise = new NoiseGenerator(_noiseFrequency);
 
-            _feet[0] = Vector3.right * _stride * +0.5f;
-            _feet[1] = Vector3.right * _stride * -0.5f;
+            // Initial foot positions
+            var origin = SetY(transform.position, 0);
+            var foot = transform.right * _stride / 2;
+            _feet[0] = origin - foot;
+            _feet[1] = origin + foot;
         }
 
         void Update()
         {
-            _bodyNoise.Frequency = _noiseFrequency;
-            _bodyNoise.Step();
+            // Noise update
+            _noise.Frequency = _noiseFrequency;
+            _noise.Step();
 
+            // Check if the next step is going to begin in this frame.
             var delta = _stepFrequency * Time.deltaTime;
 
-            if (StepCount < Mathf.FloorToInt(_stepTime + delta))
+            if (StepCount < Mathf.FloorToInt(_step + delta))
             {
-                var lf2rf = (_feet[1] - _feet[0]).normalized * _stride;
-
+                // Update the next pivot point.
+                var right = (_feet[1] - _feet[0]).normalized * _stride;
                 if (PivotIsLeft)
-                    _feet[1] = _feet[0] + StepRotation * lf2rf;
+                    _feet[1] = _feet[0] + StepRotationFull * right;
                 else
-                    _feet[0] = _feet[1] - StepRotation * lf2rf;
+                    _feet[0] = _feet[1] - StepRotationFull * right;
             }
 
-            _stepTime += delta;
+            _step += delta;
 
+            // Update the chest matrices for use in OnAnimatorIK.
             var chest = _animator.GetBoneTransform(HumanBodyBones.Chest);
-            _spineMatrix = chest.localToWorldMatrix;
-            _spineMatrixInv = chest.worldToLocalMatrix;
+            _chestMatrix = chest.localToWorldMatrix;
+            _chestMatrixInv = chest.worldToLocalMatrix;
         }
 
         void OnAnimatorIK(int layerIndex)
         {
-            _animator.SetIKPosition(AvatarIKGoal.LeftFoot, LeftFootPos);
+            _animator.SetIKPosition(AvatarIKGoal.LeftFoot, LeftFootPosition);
             _animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
 
-            _animator.SetIKPosition(AvatarIKGoal.RightFoot, RightFootPos);
+            _animator.SetIKPosition(AvatarIKGoal.RightFoot, RightFootPosition);
             _animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
 
             _animator.bodyPosition = BodyPosition;
             _animator.bodyRotation = BodyRotation;
 
-            var twist = Quaternion.AngleAxis(-8, Vector3.forward) * _bodyNoise.Rotation(5, 30, 20, 20);
+            var twist = Quaternion.AngleAxis(-8, Vector3.forward) * _noise.Rotation(5, 30, 20, 20);
             _animator.SetBoneLocalRotation(HumanBodyBones.Spine, twist);
             _animator.SetBoneLocalRotation(HumanBodyBones.Chest, twist);
             _animator.SetBoneLocalRotation(HumanBodyBones.UpperChest, twist);
@@ -204,5 +265,7 @@ namespace Puppet
             _animator.SetLookAtPosition(LookAtPosition);
             _animator.SetLookAtWeight(1);
         }
+
+        #endregion
     }
 }
